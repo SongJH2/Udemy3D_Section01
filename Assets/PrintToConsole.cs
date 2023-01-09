a8b7dd6263e87124a9eac70d05af80e717e58003
SongJH2 <117159941+SongJH2@users.noreply.github.com>
2023-01-09 14:21:06 +0900

    first

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("나는 콘솔에 출력중이다. 이것은 테스트 입니다. git 수정 테스트가 될까요?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
